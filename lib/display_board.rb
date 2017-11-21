04947778c62eb3244b5b5190540a86cd6e68cf2f
Linda ONeill <oneill.linda21@gmail.com>
2017-11-21 19:25:25 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@ def display_board(board)
     print " #{board[0]} ", "|", " #{board[1]} ", "|"
     puts " #{board[2]} "
     puts "-----------"
-    print " #{board[3]}  ", "|", " #{board[4]} ", "|"
+    print " #{board[3]} ", "|", " #{board[4]} ", "|"
     puts " #{board[5]} "
     puts "-----------"
     print " #{board[6]} ", "|", " #{board[7]} ", "|"
